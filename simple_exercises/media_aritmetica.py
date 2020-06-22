620a15028193811d9f3789ecfd38f2c9f70299b7
Jose Cerván <cervnj@gmail.com>
2020-06-22 11:53:08 +0200

    Update media_aritmetica.py

--- simple_exercises/media_aritmetica.py
+++ simple_exercises/media_aritmetica.py
@@ -4,4 +4,4 @@ numeros = list()
 for num in range(cuantos):
   numeros.append(float(input('Número ' + str(num + 1) + ': ')))
 
-print('Media aritmética:', sum(numeros)/len(numeros))
+print('Media aritmética:', sum(numeros)/cuantos)
